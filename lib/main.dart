3e698bdba3f3d394a512fbd98e12917ce4430306
Sarwan <iamsarwandahri@gmail.com>
2022-01-24 20:02:15 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +17,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 5;
+  int right = 1;
+  Random rand = Random();
+  void changeState() {
+    setState(() {
+      right = rand.nextInt(6) + 1;
+      left = rand.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+            child: Image.asset('images/dice$left.png'),
+            onPressed: changeState,
+          )),
+          Expanded(
+              child: FlatButton(
+            child: Image.asset('images/dice$right.png'),
+            onPressed: changeState,
+          ))
+        ],
+      ),
+    );
   }
 }
